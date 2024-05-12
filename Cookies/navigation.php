72f6a4b5895f4e82c5cf4212b3699dc7937b7958
sivek <127289850+s1vek@users.noreply.github.com>
2024-05-12 17:16:39 +0200

    Add files via upload

---
+++ Cookies/navigation.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="black-block"></div>
+    <div class="header-content">
+        <h1 class="logo">Lorem</h1>
+        <nav class="header-nav">
+            <ul>
+                <li><a href="index.php">Shop</a></li>
+                <li><a href="profile.php">Profile</a></li>
+                <li><a href="basket.php">Basket</a></li>
+            </ul>
+        </nav>
+        <img class="hamburger-menu" src="img/hamburger-menu.png" alt="hamburger-menu">
+    </div>
+</header>
